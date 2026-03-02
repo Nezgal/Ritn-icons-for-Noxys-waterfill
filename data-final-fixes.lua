b9d2af3745b65855611475db007e77115627dbf1
Nezgal <nezgal@protonmail.com>
2026-03-02 14:57:45 +0100

    change icons and color

---
+++ data-final-fixes.lua
@@ -0,0 +1,64 @@
+data.raw.technology["waterfill"].icon = "__Ritn-icons-for-Noxys-waterfill__/graphics/waterfill.png"
+data.raw.technology["waterfill"].icon_size = 128
+data.raw.technology["waterfill"].icon_mipmaps = 5
+table.insert(data.raw.technology["waterfill"].prerequisites, "military-science-pack")
+
+data.raw.item["waterfill"].stack_size = 200
+data.raw.item["waterfill"].icon = "__Ritn-icons-for-Noxys-waterfill__/graphics/waterfill.png"
+data.raw.item["waterfill"].icon_size = 128
+data.raw.item["waterfill"].icon_mipmaps = 5
+
+data.raw.item["deepwaterfill"].stack_size = 200
+data.raw.item["deepwaterfill"].icon = nil
+data.raw.item["deepwaterfill"].icon_size = nil
+data.raw.item["deepwaterfill"].icon_mipmaps = nil
+data.raw.item["deepwaterfill"].icons = {{
+    icon = "__Ritn-icons-for-Noxys-waterfill__/graphics/waterfill.png",
+    icon_size = 128,
+    icon_mipmaps = 5,
+    tint = { a = 1,  b = 0.8, g = 0.4, r = 0 }
+}}
+
+data.raw.item["waterfill-green"].stack_size = 200
+data.raw.item["waterfill-green"].icon = nil
+data.raw.item["waterfill-green"].icon_size = nil
+data.raw.item["waterfill-green"].icon_mipmaps = nil
+data.raw.item["waterfill-green"].icons = {{
+    icon = "__Ritn-icons-for-Noxys-waterfill__/graphics/waterfill.png",
+    icon_size = 128,
+    icon_mipmaps = 5,
+    tint = { a = 1,  b = 0.4, g = 0.8, r = 0 }
+}}
+
+data.raw.item["deepwaterfill-green"].stack_size = 200
+data.raw.item["deepwaterfill-green"].icon = nil
+data.raw.item["deepwaterfill-green"].icon_size = nil
+data.raw.item["deepwaterfill-green"].icon_mipmaps = nil
+data.raw.item["deepwaterfill-green"].icons = {{
+    icon = "__Ritn-icons-for-Noxys-waterfill__/graphics/waterfill.png",
+    icon_size = 128,
+    icon_mipmaps = 5,
+    tint = { a = 1,  b = 0.2, g = 0.6, r = 0 }
+}}
+
+data.raw.item["shallowwaterfill"].stack_size = 200
+data.raw.item["shallowwaterfill"].icon = nil
+data.raw.item["shallowwaterfill"].icon_size = nil
+data.raw.item["shallowwaterfill"].icon_mipmaps = nil
+data.raw.item["shallowwaterfill"].icons = {{
+    icon = "__Ritn-icons-for-Noxys-waterfill__/graphics/waterfill.png",
+    icon_size = 128,
+    icon_mipmaps = 5,
+    tint = { a = 1,  b = 0.7, g = 1, r = 1 }
+}}
+
+data.raw.item["mudwaterfill"].stack_size = 200
+data.raw.item["mudwaterfill"].icon = nil
+data.raw.item["mudwaterfill"].icon_size = nil
+data.raw.item["mudwaterfill"].icon_mipmaps = nil
+data.raw.item["mudwaterfill"].icons = {{
+    icon = "__Ritn-icons-for-Noxys-waterfill__/graphics/waterfill.png",
+    icon_size = 128,
+    icon_mipmaps = 5,
+    tint = { a = 1,  b = 0.5, g = 0.5, r = 1 }
+}}
